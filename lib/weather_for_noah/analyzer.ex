3d658ef2f03bb06fdce2626f27ac14e6d17ff926
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2017-01-02 12:58:22 -0500

    convert float to integer to check temp range

--- lib/weather_for_noah/analyzer.ex
+++ lib/weather_for_noah/analyzer.ex
@@ -2,12 +2,12 @@ require IEx;
 defmodule WeatherForNoah.Analyzer do 
   
   def analyze(report = %{main: "Rain", temp_min:  temp_min, description: description}) 
-  when temp_min in 32..50 do 
+  when round(temp_min) in 32..50 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "cold", percipitation: "rain"})}
   end
 
   def analyze(report = %{main: "Rain", temp_min:  temp_min, description: description}) 
-  when temp_min in 50..65 do 
+  when round(temp_min) in 50..65 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "warm", percipitation: "rain"})}
   end
 
@@ -16,12 +16,12 @@ defmodule WeatherForNoah.Analyzer do
   end
 
   def analyze(report = %{main: "Drizzle", temp_min:  temp_min, description: description}) 
-  when temp_min in 32..50 do 
+  when round(temp_min) in 32..50 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "cold", percipitation: "rain"})}
   end
 
   def analyze(report = %{main: "Drizzle", temp_min:  temp_min, description: description}) 
-  when temp_min in 50..65 do 
+  when round(temp_min) in 50..65 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "warm", percipitation: "rain"})}
   end
 
@@ -30,7 +30,7 @@ defmodule WeatherForNoah.Analyzer do
   end
 
   def analyze(report = %{main: "Snow", temp_min:  temp_min, description: description}) 
-  when temp_min in 0..20 do 
+  when round(temp_min) in 0..20 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "freezing", percipitation: "snow"})}
   end
 
@@ -39,47 +39,47 @@ defmodule WeatherForNoah.Analyzer do
   end
 
   def analyze(report = %{main: "Clear", temp_min:  temp_min, description: description}) 
-  when temp_min in 0..32 do 
+  when round(temp_min) in 0..32 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "freezing", percipitation: "clear"})}
   end
 
   def analyze(report = %{main: "Clear", temp_min:  temp_min, description: description}) 
-  when temp_min in 32..50 do 
+  when round(temp_min) in 32..50 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "cold", percipitation: "clear"})}
   end
 
   def analyze(report = %{main: "Clear", temp_min:  temp_min, description: description}) 
-  when temp_min in 50..60 do 
+  when round(temp_min) in 50..60 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "warm", percipitation: "celear"})}
   end
 
   def analyze(report = %{main: "Clear", temp_min:  temp_min, description: description}) 
-  when temp_min in 60..80 do 
+  when round(temp_min) in 60..80 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "hot", percipitation: "clear"})}
   end
 
   def analyze(report = %{main: "Clear", temp_min:  temp_min, description: description}) 
-  when temp_min in 80..100 do 
+  when round(temp_min) in 80..100 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "sweltering", percipitation: "clear"})}
   end
 
   def analyze(report = %{main: "Overcast", temp_min:  temp_min, description: description}) 
-  when temp_min in 0..32 do 
+  when round(temp_min) in 0..32 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "freezing", percipitation: "clear"})}
   end
 
   def analyze(report = %{main: "Overcast", temp_min:  temp_min, description: description}) 
-  when temp_min in 32..50 do 
+  when round(temp_min) in 32..50 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "cold", percipitation: "clear"})}
   end
 
   def analyze(report = %{main: "Overcast", temp_min:  temp_min, description: description}) 
-  when temp_min in 50..60 do 
+  when round(temp_min) in 50..60 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "warm", percipitation: "celear"})}
   end
 
   def analyze(report = %{main: "Overcast", temp_min:  temp_min, description: description}) 
-  when temp_min in 60..80 do 
+  when round(temp_min) in 60..80 do 
     %{weather: report, outfit: WeatherForNoah.Outfits.outfit(%{temp: "hot", percipitation: "clear"})}
   end
 
